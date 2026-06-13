7d7f8a7f5a09600b883615821c0437d36177486f
TheFloresto <121763667+TheFloresto@users.noreply.github.com>
2026-06-13 23:04:21 +0300

    Fix typo in supported kernels message

--- anykernel.sh
+++ anykernel.sh
@@ -44,7 +44,7 @@ case $kernel_version in
     *) ksu_supported=false ;;
 esac
 
-ui_print " " "  -> Wild Kernels Supported: $ksu_supported"
+ui_print " " "  -> Nerest Kernels Supported: $ksu_supported"
 $ksu_supported || abort "  -> Non-GKI device, abort."
 
 # boot install
